c6fc827a85ef5d3ae24a07856d26af7c17dc35a8
Random <rc786986@gmail.com>
2023-11-24 15:08:05 +0530

    initiAL BY RK

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "https://random-404error.github.io/portfolio_rahul",
   plugins: [react()],
 });
