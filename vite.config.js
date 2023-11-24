8ed2ef265747131239c73e0f08eeb58ffaab97ad
Random <rc786986@gmail.com>
2023-11-24 15:30:23 +0530

    Revert "initiAL BY RK"
    This reverts commit c6fc827a85ef5d3ae24a07856d26af7c17dc35a8.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "https://random-404error.github.io/portfolio_rahul",
   plugins: [react()],
 });
